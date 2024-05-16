92ce6192bf13bfc9c3bc922748597bb2b8bd44f8
Shah Fahid <135055359+mrfahid@users.noreply.github.com>
2024-05-16 18:17:19 +0000

    add

--- blog/app/components/Navbar.tsx
+++ blog/app/components/Navbar.tsx
@@ -8,7 +8,7 @@ export default function Navbar() {
         <div className="flex justify-between items-center w-full">
           <Link href="/">
             <h1 className="text-2xl font-medium">
-              Fahid <span className="text-teal-500">Blog</span>
+              Fahid <span className="text-teal-500">Blogs</span>
             </h1>
           </Link>
 
